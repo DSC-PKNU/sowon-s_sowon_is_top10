7d804e727de5590afd4848b556a90151435786c4
Dae12-Han <drakaina10@gmail.com>
2021-02-04 13:44:48 +0900

    inital commit

---
+++ app/src/main/java/com/example/main_home/Write.java
@@ -0,0 +1,14 @@
+package com.example.main_home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Write extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+    }
+}
---
+++ app/src/main/java/com/example/main_home/explain_page2.java
@@ -0,0 +1,14 @@
+package com.example.main_home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class explain_page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explain_page2);
+    }
+}
